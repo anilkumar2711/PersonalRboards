1fb6609ec3f9eeca30b5240bd8dce77ad0c05abb
Partha Preetham Krishna M L <partha.krishna@setindia.com>
2024-11-12 10:17:28 +0530

    up

--- src/pages/project/ProjectDetailsPanel.jsx
+++ src/pages/project/ProjectDetailsPanel.jsx
@@ -22,16 +22,34 @@ export default function ProjectDetailsPanel(props) {
         { value: "Done", label: "Done", color: "#C9DEFF" },
         { value: "Planning", label: "Planning", color: "#FDD13A47" }
     ];
-    const renderStatusOption = (props, option) => (
-        <li {...props} key={option.value} style={{ backgroundColor: option.color }} >
+    const priorityOptions = [{ value: "Urgent", label: "Urgent", color: "#F81111" },
+        { value: "High", label: "High", color: "#24A249" },
+        { value: "Medium", label: "Medium", color: "#9266F5" },
+        { value: "Low", label: "Low", color: "#B3ABAB" }
+    ];
+    const labelOptions = [
+        { value: "Label 1", label: "Label 1", color: "#F81111" },
+        { value: "Label 2", label: "Label 2", color: "#24A249" },
+        { value: "Label 3", label: "Label 3", color: "#9266F5" },
+        { value: "Label 4", label: "Label 4", color: "#B3ABAB" }
+    ];
+
+    const renderColorOption = (props, option) => (
+        <li {...props} key={option.value} >
+            <span style={{backgroundColor:option.color, borderRadius:'4px', width:'16px',height:'16px', marginRight:'10px'}} ></span>
             {option.label}
         </li>
     );
-    const priorityOptions = [{ value: "Urgent", label: "Urgent", color: "#F81111" },
-    { value: "High", label: "High", color: "#24A249" },
-    { value: "Medium", label: "Medium", color: "#9266F5" },
-    { value: "Low", label: "Low", color: "#B3ABAB" }
-    ]
+
+    const renderIconOption = (props, option) => (
+        <li {...props} key={option.value} >
+            <span style={{backgroundColor:option.color,padding:'4px',display:'flex',gap:1,whiteSpace:'nowrap',borderRadius:'4px'}} >
+                <Flag sx={{ height: 16, width: 16, color: 'text.secondary', marginLeft:'10px' }} />
+                {option.label}
+            </span>
+        </li>
+    );
+    
 
     const [relatedProjects] = useState([
         { name: "Sample Project 1", status: "In Progress", owner: "Dr. Divakar Sadam", priority: "Medium", },
@@ -148,7 +166,7 @@ export default function ProjectDetailsPanel(props) {
                                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, fontSize: '12px', color: '#565656', fontWeight: '600' }}>
                                     <Clock sx={{ height: 16, width: 16, color: 'text.secondary' }} />
                                     <span style={{ fontSize: '12px', fontWeight: '600', width: 96 }}>Status</span>
-                                    <Input name="status" type="search" options={statusOptions} renderOption={renderStatusOption} value={project.status} sx={{ height: 32, fontSize: '12px', fontWeight: '400', backgroundColor: 'yourCustomColor' }} />
+                                    <Input name="status" type="search" options={statusOptions} renderOption={renderColorOption} value={project.status} sx={{ height: 32, fontSize: '12px', fontWeight: '400', backgroundColor: 'yourCustomColor' }} />
                                 </Box>
 
                                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, fontSize: '12px', color: '#565656', fontWeight: '600' }}>
@@ -166,13 +184,13 @@ export default function ProjectDetailsPanel(props) {
                                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, fontSize: '12px', color: '#565656', fontWeight: '600' }}>
                                     <Flag sx={{ height: 16, width: 16, color: 'text.secondary' }} />
                                     <span style={{ fontSize: '0.875rem', width: 96 }}>Priority</span>
-                                    <Input name="priority" type="search" options={priorityOptions} renderOption={renderStatusOption} value={project.priority} sx={{ height: 32, fontSize: '12px', fontWeight: '400', width: '100%' }} />
+                                    <Input name="priority" type="search" options={priorityOptions} renderOption={renderColorOption} value={project.priority} sx={{ height: 32, fontSize: '12px', fontWeight: '400', width: '100%' }} />
                                 </Box>
 
                                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, fontSize: '12px', color: '#565656', fontWeight: '600' }}>
                                     <Tag sx={{ height: 16, width: 16, color: 'text.secondary' }} />
                                     <span style={{ fontSize: '12px', color: '#565656', fontWeight: '600', width: 96 }}>Label</span>
-                                    <Input name="label" value={project.label} sx={{ height: 32, fontSize: '12px', fontWeight: '400', width: '100%' }} />
+                                    <Input name="label" value={project.label} type="search" options={labelOptions} renderOption={renderIconOption} sx={{ height: 32, fontSize: '12px', fontWeight: '400', width: '100%' }} />
                                 </Box>
 
                                 <Box sx={{ '& > * + *': { marginTop: 8 } }}>
